87c3586675566a52682a924a6ff49f7ec4a6bd95
abhishek.goyal2 <abhi25goel@gmail.com>
2018-04-23 15:42:57 +0530

    Reflection class

---
+++ src/TestReflection.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+
+public class TestReflection {
+
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		// TODO Auto-generated method stub
+
+		String x="click";
+		
+		Method method =TestReflection.class.getMethod(x, String.class);
+		
+		method.invoke(method, "reflection method") ;
+	}
+	
+	public static void click(String x)
+	
+	{
+		System.out.println("Click method" +x);
+	}
+
+}
